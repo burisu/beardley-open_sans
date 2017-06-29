11d53d956c88985764f1a2c920583e50c0072932
Brice TEXIER <brice@ekylibre.com>
2017-06-29 13:40:34 +0200

    Adds missing require file

---
+++ lib/beardley-open_sans.rb
@@ -0,0 +1,1 @@
+require 'beardley/open_sans'
--- lib/beardley/open_sans.rb
+++ lib/beardley/open_sans.rb
@@ -1,13 +1,11 @@
-require "beardley/open_sans/version"
+require 'beardley/open_sans/version'
 
 module Beardley
   module OpenSans
-
     RjbLoader.before_load do |config|
-      Dir[Pathname.new(__FILE__).dirname.join("..", "..", "vendor", "java", "*.jar")].each do |path|
+      Dir[Pathname.new(__FILE__).dirname.join('..', '..', 'vendor', 'java', '*.jar')].each do |path|
         config.classpath << File::PATH_SEPARATOR + File.expand_path(path)
       end
     end
-
   end
 end
